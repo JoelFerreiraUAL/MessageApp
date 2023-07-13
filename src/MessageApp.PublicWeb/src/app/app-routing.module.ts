15b2d438d7c5e23a58667049c3c629ec27a19bf7
Joel Ferreira <30007769@students.ual.pt>
2023-07-13 15:10:46 +0100

    Added pages

---
+++ src/MessageApp.PublicWeb/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainLayoutComponent } from './common/layout/main-layout/main-layout.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './common/layout/auth-layout/auth-layout.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+
+
+const routes: Routes = [
+  { path: '', component: MainLayoutComponent },
+  { path: 'auth', component: AuthLayoutComponent, children:[
+
+    {path: 'login', component: LoginComponent, },
+    { path: 'register', component: RegisterComponent, }
+  ] }
+
+];
+
+@NgModule({
+  declarations: [],
+  exports: [ RouterModule ],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ]
+})
+export class AppRoutingModule { }
--- src/MessageApp.PublicWeb/src/app/app.module.ts
+++ src/MessageApp.PublicWeb/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MainLayoutComponent } from './common/layout/main-layout/main-layout.component';
+import { AuthLayoutComponent } from './common/layout/auth-layout/auth-layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    AuthLayoutComponent,
+    RegisterComponent,
+    LoginComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
